6ace09aa3351fb51ef97183a63e3d8e16e362fc4
grabarcz-l <Github_1123>
2019-11-07 15:56:00 +0100

    New class added

---
+++ src/test/java/testy/Gesty_zadanie2.java
@@ -0,0 +1,22 @@
+package testy;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class Gesty_zadanie2 {
+
+    WebDriver driver;
+    WebDriverWait wait;
+    Actions actions;
+
+}
