2bb2f862da227205a688f6b891eca1f92ea833c0
grabarcz-l <grabarczyk_l@interia.pl>
2019-11-12 22:54:02 +0100

    [INFO] Extended class "Gesty_zadanie2"

--- src/test/java/testy/Gesty_zadanie2.java
+++ src/test/java/testy/Gesty_zadanie2.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
+import java.sql.Time;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -18,5 +19,22 @@ public class Gesty_zadanie2 {
     WebDriver driver;
     WebDriverWait wait;
     Actions actions;
+
+    @BeforeEach
+    public void LaunchSettings() {
+        System.setProperty("webdiver.chrome.driver", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+        actions = new Actions(driver);
+        driver.manage().timeouts().pageLoadTimeout(10,TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    public void Exit() {
+        driver.close();
+        driver.quit();
+    }
 
 }
